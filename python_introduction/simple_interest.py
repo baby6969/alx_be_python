02b79a09e3698d1ad659e15f5eb7c8dff202a7cd
baby6969 <93594782+baby6969@users.noreply.github.com>
2024-12-18 04:55:28 -0600

    Update simple_interest.py

--- python_introduction/simple_interest.py
+++ python_introduction/simple_interest.py
@@ -1,5 +1,5 @@
 principal=1000 #$1000
 rate= 0.05 #5% annual interest rate
 time= 3 # (representing 3 years)
-interest= float(principal*rate*time)
+interest= principal*rate*time
 print(f"The simple interest is {interest}")
