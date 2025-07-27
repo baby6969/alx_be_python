ce266fec3339d2502ceaf51fb40faf48118c0e38
yafet zekariyas <93594782+baby6969@users.noreply.github.com>
2025-07-27 22:02:30 +0300

    Update arithmetic_operations.py

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -1,20 +1,18 @@
 num1 = int(input('plese inpute number:'))
 num2 = int(input('plese inpute number:'))
 operation = input('please write your opratopn add , sud , mult ,div:')
-# arithmetic_operations.py
-
-# arithmetic_operations.py
-
 def perform_operation(num1, num2, operation):
-    if operation == 'add':
-        return num1 + num2
-    elif operation == 'subtract':
-        return num1 - num2
-    elif operation == 'multiply':
-        return num1 * num2
-    elif operation == 'divide':
-        if num2 == 0:
-            return "Error: Division by zero is not allowed."
-        return num1 / num2
-    else:
-        return "Error: Invalid operation."
+
+  if operation == 'add':
+    result = num1 + num2
+    return result
+  if operation == 'sud':
+    result = num1 - num2
+    return result
+  if operation == 'mult':
+    result = num1 * num2
+    return result 
+  if operation == 'div':
+    result = num1 / num2  
+    return result
+print( perform_operation(num1,num2,operation))
